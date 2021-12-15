58f469d54b268ce9220cd8f526d3f4439f881163
25Simone <dantsimone111@gmail.com>
2021-12-15 14:40:58 +0100

    snack-2 completato

---
+++ snack-2/js/script.js
@@ -0,0 +1,11 @@
+// chiedo all'utente di inserire un numero
+let num = parseInt(prompt('Inserisci un numero'));
+
+// dichiaro la variabile dell'operazione
+let cube = 0;
+
+// definisco in un ciclo for l'operazione da svolgere
+for (let i = 1; i <= num; i++) {
+    cube = i * i *i;
+    console.log(cube);
+}
